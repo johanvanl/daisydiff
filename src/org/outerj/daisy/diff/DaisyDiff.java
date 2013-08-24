8f63e61ab105369c6892e6199edbc8eda90324ab
johan <johan@johan-Inspiron-1440.(none)>
2013-08-24 11:24:27 +0200

    Usable, added the jars needed to run in /lib/

--- src/org/outerj/daisy/diff/DaisyDiff.java
+++ src/org/outerj/daisy/diff/DaisyDiff.java
@@ -15,88 +15,134 @@
  */
 package org.outerj.daisy.diff;
 
-import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Locale;
+
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.sax.SAXTransformerFactory;
+import javax.xml.transform.sax.TransformerHandler;
+import javax.xml.transform.stream.StreamResult;
 
 import org.outerj.daisy.diff.html.HTMLDiffer;
 import org.outerj.daisy.diff.html.HtmlSaxDiffOutput;
 import org.outerj.daisy.diff.html.TextNodeComparator;
 import org.outerj.daisy.diff.html.dom.DomTreeBuilder;
-import org.outerj.daisy.diff.tag.TagComparator;
-import org.outerj.daisy.diff.tag.TagDiffer;
-import org.outerj.daisy.diff.tag.TagSaxDiffOutput;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
-import org.xml.sax.XMLReader;
-import org.xml.sax.helpers.XMLReaderFactory;
+import org.xml.sax.helpers.AttributesImpl;
 
 public class DaisyDiff {
 
 	public DaisyDiff() {
 	}
 
-	/**
-	 * Diffs two html files, outputting the result to the specified consumer.
-	 */
-	public static void diffHTML(InputSource oldSource, InputSource newSource,
-			ContentHandler consumer, String prefix, Locale locale)
-			throws SAXException, IOException {
+	private ByteArrayOutputStream baos = null;
 
-		DomTreeBuilder oldHandler = new DomTreeBuilder();
-		XMLReader xr1 = XMLReaderFactory.createXMLReader();
-		xr1.setContentHandler(oldHandler);
-		xr1.parse(oldSource);
-		TextNodeComparator leftComparator = new TextNodeComparator(oldHandler,
-				locale);
-
-		DomTreeBuilder newHandler = new DomTreeBuilder();
-		XMLReader xr2 = XMLReaderFactory.createXMLReader();
-		xr2.setContentHandler(newHandler);
-		xr2.parse(newSource);
-
-		TextNodeComparator rightComparator = new TextNodeComparator(newHandler,
-				locale);
-
-		HtmlSaxDiffOutput output = new HtmlSaxDiffOutput(consumer, prefix);
-		HTMLDiffer differ = new HTMLDiffer(output);
-		differ.diff(leftComparator, rightComparator);
+	public void diffTwoFiles(String original_filename, String revised_filename)
+			throws FileNotFoundException {
+		InputStream originalStream = new FileInputStream(original_filename);
+		InputStream revisedStream = new FileInputStream(revised_filename);
+		diff(originalStream, revisedStream);
+		try {
+			originalStream.close();
+			revisedStream.close();
+		} catch (IOException e) {
+			// ignore
+		}
 	}
 
-	/**
-	 * Diffs two html files word for word as source, outputting the result to
-	 * the specified consumer.
-	 */
-	public static void diffTag(String oldText, String newText,
-			ContentHandler consumer) throws Exception {
-		consumer.startDocument();
-		TagComparator oldComp = new TagComparator(oldText);
-		TagComparator newComp = new TagComparator(newText);
-
-		TagSaxDiffOutput output = new TagSaxDiffOutput(consumer);
-		TagDiffer differ = new TagDiffer(output);
-		differ.diff(oldComp, newComp);
-		consumer.endDocument();
+	public void diffTwoStrings(String original_string, String revised_string) {
+		InputStream originalStream = new ByteArrayInputStream(
+				original_string.getBytes());
+		InputStream revisedStream = new ByteArrayInputStream(
+				revised_string.getBytes());
+		diff(originalStream, revisedStream);
+		try {
+			originalStream.close();
+			revisedStream.close();
+		} catch (IOException e) {
+			// ignore
+		}
 	}
 
-	/**
-	 * Diffs two html files word for word as source, outputting the result to
-	 * the specified consumer.
-	 */
-	public static void diffTag(BufferedReader oldText, BufferedReader newText,
-			ContentHandler consumer) throws Exception {
+	private void diff(InputStream originalstream, InputStream revisedStream) {
+		try {
+			SAXTransformerFactory tf = (SAXTransformerFactory) TransformerFactory
+					.newInstance();
 
-		TagComparator oldComp = new TagComparator(oldText);
-		TagComparator newComp = new TagComparator(newText);
+			baos = new ByteArrayOutputStream();
+			TransformerHandler result = tf.newTransformerHandler();
+			result.setResult(new StreamResult(baos));
 
-		TagSaxDiffOutput output = new TagSaxDiffOutput(consumer);
-		TagDiffer differ = new TagDiffer(output);
-		differ.diff(oldComp, newComp);
+			XslFilter filter = new XslFilter();
+
+			ContentHandler postProcess = filter.xsl(result,
+					"org/outerj/daisy/diff/htmlheader.xsl");
+
+			Locale locale = Locale.getDefault();
+			String prefix = "diff";
+
+			HtmlCleaner cleaner = new HtmlCleaner();
+
+			InputSource oldSource = new InputSource(originalstream);
+			InputSource newSource = new InputSource(revisedStream);
+
+			DomTreeBuilder oldHandler = new DomTreeBuilder();
+			cleaner.cleanAndParse(oldSource, oldHandler);
+			TextNodeComparator leftComparator = new TextNodeComparator(
+					oldHandler, locale);
+
+			DomTreeBuilder newHandler = new DomTreeBuilder();
+			cleaner.cleanAndParse(newSource, newHandler);
+			TextNodeComparator rightComparator = new TextNodeComparator(
+					newHandler, locale);
+
+			postProcess.startDocument();
+			postProcess.startElement("", "diffreport", "diffreport",
+					new AttributesImpl());
+			postProcess.startElement("", "diff", "diff", new AttributesImpl());
+			HtmlSaxDiffOutput output = new HtmlSaxDiffOutput(postProcess,
+					prefix);
+
+			HTMLDiffer differ = new HTMLDiffer(output);
+			differ.diff(leftComparator, rightComparator);
+
+			postProcess.endElement("", "diff", "diff");
+			postProcess.endElement("", "diffreport", "diffreport");
+			postProcess.endDocument();
+
+		} catch (Throwable e) {
+			e.printStackTrace();
+			if (e.getCause() != null) {
+				e.getCause().printStackTrace();
+			}
+			if (e instanceof SAXException) {
+				((SAXException) e).getException().printStackTrace();
+			}
+		}
 	}
 
-	public static void main(String[] args) {
+	public String getHTML() {
+		return baos.toString();
+	}
 
+	public static void main(String[] args) throws SAXException, IOException {
+		DaisyDiff dd = new DaisyDiff();
+		//String original = "/home/johan/Desktop/test_html/start - CS 748_old.html";
+		//String revised = "/home/johan/Desktop/test_html/start - CS 748_new.html";
+		//dd.diffTwoFiles(original, revised);
+		
+		String or = "<html>hello</html>";
+		String re = "<html>hello maatjies</html>";
+		dd.diffTwoStrings(or, re);
+		
+		System.out.println(dd.getHTML());
 	}
 
 }
